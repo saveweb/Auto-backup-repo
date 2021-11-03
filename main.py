2821b71b10ac525bd00b4a4fe1b7dfe62aef7c4c
yzqzss <30341059+yzqzss@users.noreply.github.com>
2021-11-03 08:15:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# with open('repos.csv', 'r') as f:
+#     file_content = f.read()
+#     lines = file_content.split('\n')
+
+# for line in lines[1:]:
+#     line = line.strip()
+#     if not line:
+#         continue
+#     parts = line.split(',')
+#     print(len(parts))
+#     # if len(parts) != 4:
+#     #     continue
+#     parts = [part.strip() for part in parts]
+#     print(parts)
