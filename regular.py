cffbbd017809edd964d4ee791071bf0599db5735
yzqzss <30341059+yzqzss@users.noreply.github.com>
2021-11-21 12:14:03 +0800

    Update regular.py

--- regular.py
+++ regular.py
@@ -11,3 +11,15 @@ lines.sort()
 with open('repos.csv', 'w') as f:
   for line in lines:
     f.write(line.replace("https://", "").replace("/", ",")+'\n')
+##懒得写 while 循环，直接复制粘贴一遍，233:
+with open('repos.csv', 'r') as f:
+  file = f.read()
+lines = file.splitlines()
+lines = set(lines)
+lines.discard('')
+lines = list(lines)
+lines.sort()
+
+with open('repos.csv', 'w') as f:
+  for line in lines:
+    f.write(line.replace("https://", "").replace("/", ",")+'\n')
