4981533593c9fe93593a907affd4da5019d61ace
yzqzss <30341059+yzqzss@users.noreply.github.com>
2022-01-01 13:12:22 +0800

    Rere-fix | use shutil to delete folder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 print('自我更新...(除/repos_data/目录外，所有文件将被远端替代)')
 isupdate = os.system('git fetch --all && git reset --hard origin/main && git pull --force')
@@ -43,13 +44,14 @@ for line in lines:
     repo_url = 'https://saveweb:fake_password@'+repo_inf[0]+'/'+repo_inf[1]+'/'+repo_inf[2]
     clone_command = 'git clone --mirror '+repo_url+' repos_data/'+repo_inf[1]+'/'+repo_inf[2]+'.git'
     try:
-        clean_folder = os.removedirs('repos_data/'+repo_inf[1]+'/'+repo_inf[2]) #清空文件夹
+        shutil.rmtree('repos_data/'+repo_inf[1]+'/'+repo_inf[2]+'.git') #清空文件夹
     finally:
         log = os.system(clone_command)
         if log == 0:
             print('成功Clone!')
         else:
             print('Clone时发生未知错误，返回值:'+str(log))
+        continue
   if isrepo == True:
     print('Git目录存在，开始更新...')
     repo_dir = 'repos_data/'+repo_inf[1]+'/'+repo_inf[2]+'.git'
